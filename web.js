1980e9852f13c534683e46189c2d82bcf043d6f4
JP <zpilot9@gmail.com>
2013-07-10 15:35:04 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,20 +2,9 @@
 var fs = require("fs");
 var fileName = "index.html";
 
-fs.exists(fileName, function(exists) {
-    if (exists) {
-	fs.stat(fileName, function(error, stats) {
-            fs.open(fileName, "r", function(error, fd) {
-		var buffer = new Buffer(stats.size);
-                fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
-                    var data = buffer.toString("utf8", o, buffer.length);
-                    console.log(data);
-                    fs.close(fd);
-                });
-	    });
-        });
-    }
-});
+var data = fs.readFileSync(fileName, "utf8");
+console.log(data);
+
 
 
 // var app = express.createServer(express.logger());
