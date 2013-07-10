2f2ac37fa3bbb7206eb31f2b62f9e2ebde7c5bc9
JP <zpilot9@gmail.com>
2013-07-10 15:17:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,32 @@
 var express = require('express');
+var fs = require("fs");
+var fileName = "/index.html";
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.exists(fileName, function(exists) {
+    if (exists) {
+	fs.stat(fileName, function(error, stats) {
+            fs.open(fileName, "r", function(error, fd) {
+		var buffer = new Buffer(stats.size);
+                fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
+                    var data = buffer.toString("utf8", o, buffer.length);
+                    console.log(data);
+                    fs.close(fd);
+                });
+	    });
+        });
+    }
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+// var app = express.createServer(express.logger());
+
+// app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+// });
+
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//  console.log("Listening on " + port);
+// });
 
 
