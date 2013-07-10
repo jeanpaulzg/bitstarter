3369d3a2b820027b312904a537c65ec2440f5ab3
JP <zpilot9@gmail.com>
2013-07-10 15:36:49 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -13,9 +13,9 @@ console.log(data);
 //  response.send('Hello World 2!');
 // });
 
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//  console.log("Listening on " + port);
-// });
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 
