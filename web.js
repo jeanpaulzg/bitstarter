84b2df2354c43c0978d711a90107018e7fff8a4c
JP <zpilot9@gmail.com>
2013-07-10 15:30:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 // var express = require('express');
 var fs = require("fs");
-var fileName = "/index.html";
+var fileName = "index.html";
 
 fs.exists(fileName, function(exists) {
     if (exists) {
